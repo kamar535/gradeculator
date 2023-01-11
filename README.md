dff0114ca6a171b2ee32695e77e8a3177ed6cf2c
kamar535 <karl.marklund@gmail.com>
2023-01-11 16:51:42 +0100

    OSPP and DSP versions available

---
+++ README.md
@@ -0,0 +1,115 @@
+<br/>
+
+<img src="images/logo.png"/>
+
+GradeCulator is a final grade calculator for courses where the final grade
+is calculated as a weighed average of a set of the grades for the Ladok modules
+of the course. The calculator can for example be added to a page in
+Studium/Canvas to makes it easy for students to understand how each of the Ladok
+module grades affects the final grade. The calculator also makes it easy to see
+how an indivdual assignment grade affects the final grade. 
+
+# Example
+
+In the below example from the OSPP (1DT096) course, the final grade is
+calculated as a weighted average of all the Ladok modules with grade scale Fail,
+3, 4, 5 with the number of credits for each module as weight. 
+
+<p align="center">
+<img src="images/ospp-example.png" width="400px"/>
+</p>
+
+The Assignments (part 1) module consists of three graded assignmetns and the grade
+for this module is the average of the grades for these three assignments. In
+this example (3 + 3 + 3)/3 ≈ 3.00 = 3.
+
+Each of the reminding moudles only has a single graded artifact. 
+
+The final grade is calculated as follows: 
+(2\*3 + 1\*3 + 1\*3 + 2\*3 + 5\*3) / (2 + 1 + 1 + 5 + 2) =
+33/11 ≈ 3.00 ≈ 3.
+
+If one or more of the graded artifacts has the grade **Fail**, no final grade can be
+calculated shown in the below example. 
+
+<p align="center"r>
+<img src="images/ospp-no-grade-example.png" width="400">
+</p>
+
+# Demo
+
+Check out this [demo](demo/gradeculator.html) of the GradeCulator confiugred for
+the OSPP (1DT096) course.  
+
+# Implemented in Elm
+
+The final grade calculator is implemented in [Elm][elm], a
+small [statically typed][types] functional language with [algebraic data
+types][custom-data-types] and [pattern-matching][pattern-matching].
+Elm [compiles][elm-make] to JavaScript.
+
+[elm]: https://elm-lang.org/
+[types]: https://guide.elm-lang.org/types/
+[custom-data-types]: https://guide.elm-lang.org/types/custom_types.html
+[pattern-matching]: https://guide.elm-lang.org/types/pattern_matching.html
+[elm-make]: https://guide.elm-lang.org/install/elm.html#elm-make
+
+# Create Elm App
+
+This project is bootstrapped with [Create Elm
+App](https://github.com/halfzebra/create-elm-app). Read more in the [CreateElmApp.md](CreateElmApp.md).
+
+## Development mode
+
+To run the app in the development mode, execute the following command in the
+terminal from the project root directory. 
+
+```
+elm-app run
+```
+
+# Embed in Studium
+
+Follow these steps to embed the GradeCulator to a course in Studium. 
+
+## Compile to Javascript
+
+```
+elm make src/Main.elm --output=studium/gradeculator.js
+```
+
+## Upload to Studium
+
+Upload `studium/gradeculator.html` and `studium/gradeculator.js` to the files area in Studium.
+
+## Embed the GradeCulator on a Studium/Canas page
+
+This solution is based on https://canvas.ou.edu/courses/56095
+
+
+In the files area, click on `gradeculator.html` to preview the file. In the
+address bar you will see something similar to this:
+
+``` shell
+https://uppsala.instructure.com/courses/55113/files/folder/GradeCulator?preview=2836960
+                                        -----                                         -------
+                                          ^                                              ^
+                                          |                                              |               
+                                          |                                              |
+                                         
+                                      Course ID                                       File ID
+```
+
+Use the `Course ID` and `File ID` to create an `<iframe>`.
+```
+<iframe src="https://uppsala.instructure.com/courses/<Course ID>/files/<File
+ID>/download" width="450" height="320"
+data-api-endpoint="https://uppsala.instructure.com/api/v1/courses/<Course
+ID>/files/<File ID>" data-api-returntype="File"></iframe>
+```
+
+, for example:
+
+``` shell
+<iframe src="https://uppsala.instructure.com/courses/55113/files/2836960/download" width="450" height="320" data-api-endpoint="https://uppsala.instructure.com/api/v1/courses/55113/files/2836960" data-api-returntype="File"></iframe>
+```
---
+++ src/Main.elm
@@ -0,0 +1,710 @@
+module Main exposing (..)
+
+-- TO READ: https://thoughtbot.com/blog/running-out-of-maps
+
+import Array
+import Array.Extra
+import Browser
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html, br, i)
+import List
+import Maybe.Extra
+import Round
+import String
+import Tuple2
+
+
+type Grade
+    = Fail
+    | Three
+    | Four
+    | Five
+
+
+gradeToString : Grade -> String
+gradeToString grade =
+    case grade of
+        Fail ->
+            "F"
+
+        Three ->
+            "3"
+
+        Four ->
+            "4"
+
+        Five ->
+            "5"
+
+
+type alias Module =
+    { code : String
+    , name : String
+    , credits : Int
+    , grades : Grades
+    }
+
+
+type Grades
+    = SingleGrade Grade
+    | Grades (Array.Array ModulePart)
+
+
+type alias ModulePart =
+    { name : String
+    , grade : Grade
+    }
+
+
+gradeToMaybeFloat : Grade -> Maybe Float
+gradeToMaybeFloat grade =
+    case grade of
+        Fail ->
+            Nothing
+
+        Three ->
+            Just 3
+
+        Four ->
+            Just 4
+
+        Five ->
+            Just 5
+
+
+moduleWeightAndAveragageGradeFromIndex : Int -> Array.Array Module -> Maybe ( Int, Maybe Float )
+moduleWeightAndAveragageGradeFromIndex moduleIndex modules =
+    Array.get moduleIndex modules
+        |> Maybe.map (\m -> ( m.credits, averageModuleGrade m ))
+
+
+viewModuleAverageWeightedGrade : Int -> Array.Array Module -> Element Msg
+viewModuleAverageWeightedGrade moduleIndex modules =
+    case moduleWeightAndAveragageGradeFromIndex moduleIndex modules of
+        Nothing ->
+            text <| "Error: Invalid module index " ++ String.fromInt moduleIndex
+
+        Just ( _, Nothing ) ->
+            text "No grade"
+
+        Just ( w, Just avg ) ->
+            text <| String.fromInt <| w * round avg
+
+
+viewAverageModuleGrade : Int -> Array.Array Module -> Element Msg
+viewAverageModuleGrade moduleIndex modules =
+    case averageModuleGradeFromIndex moduleIndex modules of
+        Nothing ->
+            text <| "Error: Invalid module index " ++ String.fromInt moduleIndex
+
+        Just Nothing ->
+            text "No grade"
+
+        Just (Just avg) ->
+            text <| (String.fromInt <| round avg) ++ " (" ++ Round.round 2 avg ++ ")"
+
+
+averageModuleGradeFromIndex : Int -> Array.Array Module -> Maybe (Maybe Float)
+averageModuleGradeFromIndex moduleIndex modules =
+    Array.get moduleIndex modules |> Maybe.map averageModuleGrade
+
+
+averageModuleGrade : Module -> Maybe Float
+averageModuleGrade m =
+    averageGrade m.grades
+
+
+averageGrade : Grades -> Maybe Float
+averageGrade grades =
+    case grades of
+        SingleGrade g ->
+            gradeToMaybeFloat g
+
+        Grades gs ->
+            Array.toList gs
+                |> List.map .grade
+                |> Maybe.Extra.traverse gradeToMaybeFloat
+                |> Maybe.map List.sum
+                |> Maybe.map (\tot -> tot / toFloat (Array.length gs))
+
+
+finalGrade : Array.Array { a | credits : Int, grades : Grades } -> Maybe ( Int, Int )
+finalGrade modules =
+    Array.toList modules
+        |> List.map (\mod -> ( .credits mod, .grades mod ) |> Tuple2.maybeMapSecond averageGrade)
+        |> Maybe.Extra.combine
+        |> Maybe.map (List.foldl (\( w, g ) ( wSum, gSum ) -> ( w + wSum, (toFloat w * g) + gSum )) ( 0, 0 ))
+        |> Maybe.map (\( totalWeight, sum ) -> ( totalWeight, round sum ))
+
+
+defaultGrade : Grade
+defaultGrade =
+    Three
+
+
+initPart : String -> ModulePart
+initPart n =
+    { name = n
+    , grade = defaultGrade
+    }
+
+
+singleGrade : Grades
+singleGrade =
+    SingleGrade defaultGrade
+
+
+multipleGrades : List String -> Grades
+multipleGrades names =
+    Grades <| initModuleParts names
+
+
+
+--- OSPP Modules
+
+
+a1 : Module
+a1 =
+    { code = "2000"
+    , name = "Assignments (part 1)"
+    , credits = 2
+    , grades = multipleGrades [ "Fundamental concepts", "The process concept", "Threads, synchronization and deadlock" ]
+    }
+
+
+e : Module
+e =
+    { code = "3000"
+    , name = "Written exam (part 1)"
+    , credits = 1
+    , grades = singleGrade
+    }
+
+
+a2 : Module
+a2 =
+    { code = "5000"
+    , name = "Assignments (part 2)"
+    , credits = 1
+    , grades = singleGrade
+    }
+
+
+pg : Module
+pg =
+    { code = "6000"
+    , name = "Project (group)"
+    , credits = 2
+    , grades = singleGrade
+    }
+
+
+pi : Module
+pi =
+    { code = "7000"
+    , name = "Project (individual)"
+    , credits = 5
+    , grades = singleGrade
+    }
+
+
+osppModules : Array.Array Module
+osppModules =
+    Array.fromList [ a1, e, a2, pg, pi ]
+
+
+
+--- DSP Modules
+--- OSPP Modules
+
+
+dspAssignments : Module
+dspAssignments =
+    { code = "2010"
+    , name = "Assignments"
+    , credits = 5
+    , grades = multipleGrades [ "Fundamental concepts", "The process concept", "Threads, synchronization and deadlock" ]
+    }
+
+
+dspExam : Module
+dspExam =
+    { code = "1010"
+    , name = "Written exam"
+    , credits = 2
+    , grades = singleGrade
+    }
+
+
+dspProjectGroup : Module
+dspProjectGroup =
+    { code = "3020"
+    , name = "Project (group)"
+    , credits = 2
+    , grades = singleGrade
+    }
+
+
+dspProjectIndividual : Module
+dspProjectIndividual =
+    { code = "3010"
+    , name = "Project (individual)"
+    , credits = 5
+    , grades = singleGrade
+    }
+
+
+dspModules =
+    Array.fromList [ dspExam, dspAssignments, dspProjectIndividual, dspProjectGroup ]
+
+
+
+---
+
+
+initModuleParts : List String -> Array.Array ModulePart
+initModuleParts names =
+    Array.fromList <| List.map initPart names
+
+
+type alias Model =
+    Array.Array Module
+
+
+type ButtonPosition
+    = First
+    | Middle
+    | Last
+
+
+type Msg
+    = SetModuleGrade Int Grade
+    | SetPartGrade Int Int Grade
+
+
+viewFinalGrade : Array.Array { a | credits : Int, grades : Grades } -> Element Msg
+viewFinalGrade model =
+    finalGrade model
+        |> Maybe.map
+            (\( totalWeight, sum ) ->
+                let
+                    avgGrade =
+                        toFloat sum / toFloat totalWeight
+                in
+                paragraph [ Font.alignLeft ]
+                    [ el [ Font.bold ] <| text "Final grade "
+                    , text <| " = "
+                    , el [ Font.bold ] <| text <| String.fromInt (round avgGrade)
+                    , text <| " (" ++ String.fromInt sum
+                    , text <|
+                        "/"
+                            ++ String.fromInt totalWeight
+                            ++ " ≈ "
+                            ++ Round.round 2 avgGrade
+                            ++ ")"
+                    ]
+            )
+        |> Maybe.withDefault (paragraph [ Font.alignLeft, Font.bold ] [ text "No final grade" ])
+
+
+averageGradeToString : Maybe Float -> String
+averageGradeToString avg =
+    avg |> Maybe.map (\grade -> String.fromInt (round grade) ++ " (" ++ Round.round 2 grade ++ ")") |> Maybe.withDefault "No Grade"
+
+
+view : Model -> Html Msg
+view model =
+    layout [ padding 20 ] <|
+        column [ spacing 20, Font.size 12 ]
+            [ modulesTable model
+            , viewFinalGrade model
+            ]
+
+
+type GradeIndex
+    = ModuleIndex Int
+    | ModuleAndPartIndex Int Int
+
+
+getSingleGrade : Grades -> Maybe Grade
+getSingleGrade grade =
+    case grade of
+        SingleGrade g ->
+            Just g
+
+        Grades _ ->
+            Nothing
+
+
+getGrades : Grades -> Maybe (Array.Array ModulePart)
+getGrades grades =
+    case grades of
+        Grades gs ->
+            Just gs
+
+        SingleGrade _ ->
+            Nothing
+
+
+gradeButtons : GradeIndex -> Array.Array Module -> Element Msg
+gradeButtons gradeIndex modules =
+    let
+        option : Grade -> ButtonPosition -> Input.Option Grade msg
+        option g position =
+            Input.optionWith g (gradeButton g position (gradeToString g))
+
+        grade : Maybe Grade
+        grade =
+            case gradeIndex of
+                ModuleIndex i ->
+                    Maybe.map .grades (Array.get i modules)
+                        |> Maybe.andThen getSingleGrade
+
+                ModuleAndPartIndex m p ->
+                    Array.get m modules
+                        |> Maybe.map .grades
+                        |> Maybe.andThen getGrades
+                        |> Maybe.andThen (Array.get p)
+                        |> Maybe.map .grade
+
+        msg =
+            case gradeIndex of
+                ModuleIndex moduleIndex ->
+                    SetModuleGrade moduleIndex
+
+                ModuleAndPartIndex moduleIndex partIndex ->
+                    SetPartGrade moduleIndex partIndex
+    in
+    Input.radioRow
+        [ Border.rounded 6
+        ]
+        { onChange = msg
+        , selected = grade
+        , label =
+            Input.labelHidden "Select grade"
+        , options =
+            [ option Fail First
+            , option Three Middle
+            , option Four Middle
+            , option Five Last
+            ]
+        }
+
+
+gradeButton : Grade -> ButtonPosition -> String -> Input.OptionState -> Element msg
+gradeButton grade position label state =
+    let
+        r =
+            4
+
+        b =
+            1
+
+        borders =
+            case position of
+                First ->
+                    { left = b, right = b, top = b, bottom = b }
+
+                Middle ->
+                    { left = 0, right = b, top = b, bottom = b }
+
+                Last ->
+                    { left = 0, right = b, top = b, bottom = b }
+
+        corners =
+            case position of
+                First ->
+                    { topLeft = r, bottomLeft = r, topRight = 0, bottomRight = 0 }
+
+                Middle ->
+                    { topLeft = 0, bottomLeft = 0, topRight = 0, bottomRight = 0 }
+
+                Last ->
+                    { topLeft = 0, bottomLeft = 0, topRight = r, bottomRight = r }
+    in
+    el
+        [ paddingEach { left = 6, right = 6, top = 3, bottom = 3 }
+        , Font.size 12
+        , Border.roundEach corners
+        , Border.widthEach borders
+        , Border.color color.gray
+        , Background.color <|
+            if state == Input.Selected then
+                case grade of
+                    Fail ->
+                        color.red
+
+                    _ ->
+                        color.green
+
+            else
+                color.white
+        , Font.color <|
+            if state == Input.Selected then
+                color.white
+
+            else
+                color.black
+        ]
+    <|
+        el [ centerX, centerY ] <|
+            text label
+
+
+{-| TODO: Add module : Int and part : Int fields to the Single and Part records
+and module : Int to the Header record in order to
+index into the model grades array.
+-}
+type Row
+    = Header { code : String, credits : Int, name : String, moduleIndex : Int }
+    | Single { code : String, credits : Int, name : String, moduleIndex : Int }
+    | Part { name : String, moduleIndex : Int, partIndex : Int }
+    | Sum
+
+
+getNameAndGrade : Int -> Array.Array ModulePart -> ( String, Grade )
+getNameAndGrade index parts =
+    case Array.get index parts of
+        Nothing ->
+            ( "Unknown", Three )
+
+        Just part ->
+            ( part.name, part.grade )
+
+
+
+-- NOTE: Using Array map (or indexedMap) there is no need to deal with Maybe
+-- values when accessing the elements of the array.
+
+
+moduleToRows : Int -> Module -> List Row
+moduleToRows moduleIndex m =
+    case m.grades of
+        SingleGrade _ ->
+            [ Single { code = m.code, credits = m.credits, name = m.name, moduleIndex = moduleIndex } ]
+
+        Grades grades ->
+            Header { code = m.code, credits = m.credits, name = m.name, moduleIndex = moduleIndex }
+                :: Array.Extra.indexedMapToList
+                    (\i part ->
+                        Part { name = part.name, moduleIndex = moduleIndex, partIndex = i }
+                    )
+                    grades
+
+
+modulesToRows : Array.Array Module -> List Row
+modulesToRows ms =
+    (Array.Extra.indexedMapToList
+        (\index mod -> moduleToRows index mod)
+        ms
+        |> List.concat
+    )
+        ++ [ Sum ]
+
+
+cell : Attribute msg -> Element msg -> Element msg
+cell alignment content =
+    el [ alignment, centerY, paddingXY 20 10 ] content
+
+
+sumCell : List (Attribute msg) -> Element msg -> Element msg
+sumCell style content =
+    el
+        (style
+            ++ [ Border.widthEach { top = 2, bottom = 0, left = 0, right = 0 }, paddingXY 20 10 ]
+        )
+        content
+
+
+codeColumn : Row -> Element msg
+codeColumn row =
+    case row of
+        Single record ->
+            .code record |> text |> cell Font.alignLeft
+
+        Header record ->
+            .code record |> text |> cell Font.alignLeft
+
+        Part _ ->
+            none
+
+        Sum ->
+            sumCell [] none
+
+
+weightColumn : Array.Array Module -> Row -> Element msg
+weightColumn modules row =
+    case row of
+        Single record ->
+            .credits record |> String.fromInt |> text |> cell Font.center
+
+        Header record ->
+            .credits record |> String.fromInt |> text |> cell Font.center
+
+        Part _ ->
+            none
+
+        Sum ->
+            let
+                totalWeight =
+                    Array.foldl (\m sumW -> m.credits + sumW) 0 modules
+            in
+            sumCell [ Font.center ] <| text <| String.fromInt <| totalWeight
+
+
+type alias PaddingEach =
+    { left : Int
+    , right : Int
+    , top : Int
+    , bottom : Int
+    }
+
+
+zeroPadding : PaddingEach
+zeroPadding =
+    { left = 0, right = 0, top = 0, bottom = 0 }
+
+
+nameColumn : Row -> Element msg
+nameColumn r =
+    case r of
+        Single record ->
+            .name record |> text |> cell Font.alignLeft
+
+        Header record ->
+            paragraph [ Font.alignLeft, paddingXY 20 10 ] [ .name record |> text ]
+
+        Part record ->
+            row [ paddingEach { zeroPadding | left = 20, top = 6 } ] [ paragraph [ Font.alignLeft, paddingXY 20 6 ] [ .name record |> text ] ]
+
+        Sum ->
+            sumCell [] none
+
+
+gradeColumn : Array.Array Module -> Row -> Element Msg
+gradeColumn modules r =
+    case r of
+        Single mod ->
+            cell centerX <| gradeButtons (ModuleIndex mod.moduleIndex) modules
+
+        Header h ->
+            cell Font.center <| viewAverageModuleGrade h.moduleIndex modules
+
+        Part part ->
+            cell centerX <| gradeButtons (ModuleAndPartIndex part.moduleIndex part.partIndex) modules
+
+        Sum ->
+            sumCell [ Font.bold, Font.alignRight ] <| text "Sum"
+
+
+weightedColumn : Array.Array Module -> Row -> Element Msg
+weightedColumn modules w =
+    case w of
+        Single s ->
+            cell Font.center <| viewModuleAverageWeightedGrade s.moduleIndex modules
+
+        Header h ->
+            cell Font.center <| viewModuleAverageWeightedGrade h.moduleIndex modules
+
+        Part _ ->
+            none
+
+        Sum ->
+            let
+                sum =
+                    finalGrade modules
+                        |> Maybe.map (\( _, s ) -> String.fromInt s)
+                        |> Maybe.withDefault "Incomplete"
+            in
+            sumCell [ Font.center ] <| text sum
+
+
+modulesTable : Array.Array Module -> Element Msg
+modulesTable modules =
+    let
+        header alignment txt =
+            el [ alignment, Font.bold, Border.widthEach { top = 0, bottom = 2, left = 0, right = 0 }, paddingXY 20 6 ] <| paragraph [] [ text txt ]
+
+        header2 alignment content =
+            el [ alignment, height (px 50), Font.bold, Border.widthEach { top = 0, bottom = 2, left = 0, right = 0 }, paddingXY 20 6 ] <| content
+    in
+    table []
+        { data = modulesToRows modules -- osppModules
+        , columns =
+            [ { header = header2 Font.alignLeft <| paragraph [] [ text "", html <| br [] [], text "Code" ]
+              , width = shrink
+              , view = codeColumn
+              }
+            , { header = header2 Font.alignLeft <| paragraph [] [ text "", html <| br [] [], text "Ladok module" ]
+              , width = fill
+              , view = nameColumn
+              }
+            , { header = header2 Font.center <| paragraph [] [ text "", html <| br [] [], text "Grade" ]
+              , width = shrink
+              , view = gradeColumn modules
+              }
+            , { header = header2 Font.center <| paragraph [] [ text "Weight", html <| br [] [], text "(credits)" ]
+              , width = shrink
+              , view = weightColumn modules
+              }
+            , { header = header2 Font.center <| paragraph [] [ text "Weighted", html <| br [] [], text "grade" ]
+              , width = shrink
+              , view = weightedColumn modules
+              }
+            ]
+        }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SetModuleGrade moduleIndex grade ->
+            Array.Extra.update moduleIndex
+                (\mod -> { mod | grades = SingleGrade grade })
+                model
+
+        SetPartGrade moduleIndex partIndex grade ->
+            Array.Extra.update moduleIndex
+                (\mod ->
+                    case mod.grades of
+                        SingleGrade _ ->
+                            mod
+
+                        Grades grades ->
+                            { mod
+                                | grades =
+                                    Grades
+                                        (Array.Extra.update
+                                            partIndex
+                                            (\part -> { part | grade = grade })
+                                            grades
+                                        )
+                            }
+                )
+                model
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = osppModules
+        , view = view
+        , update = update
+        }
+
+
+color =
+    { blue = rgb255 0x72 0x9F 0xCF
+    , red = rgb255 255 0 0
+    , green = rgb255 0 128 0
+    , black = rgb255 0 0 0
+    , gray = rgb255 0x7F 0x7F 0x7F
+    , darkCharcoal = rgb255 0x2E 0x34 0x36
+    , lightBlue = rgb255 0xC5 0xE8 0xF7
+    , lightGrey = rgb255 0xE0 0xE0 0xE0
+    , white = rgb255 0xFF 0xFF 0xFF
+    }
